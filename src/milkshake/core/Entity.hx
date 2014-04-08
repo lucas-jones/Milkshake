21bd8085c89107d06591ee5183f7309c773b42e1
Lucas Jones <Necxii@gmail.com>
2014-04-08 11:52:54 +0100

    Parent node stuff

--- src/milkshake/core/Entity.hx
+++ src/milkshake/core/Entity.hx
@@ -1,13 +1,39 @@
 package milkshake.core;
 
+import milkshake.core.Node;
+import milkshake.game.scene.Scene;
+
 class Entity extends Node
 {
+	public var scene(default, null):Scene;
+	
 	@:isVar public var x(get, set):Float;
 	@:isVar public var y(get, set):Float;
 	
 	public function new(id:String="undefined-entity") 
 	{
 		super(id);
+	}
+	
+	public function setScene(scene:Scene):Void
+	{
+		this.scene = scene;
+	}
+	
+	override public function addNode(node:Node):Void 
+	{
+		if (Std.is(node, Entity))
+		{
+			trace("Set Scene");
+			trace(scene);
+			var entity:Entity = cast node;
+			
+			entity.setScene(scene);
+			
+			scene.onNodeAdded.dispatch(node);
+		}
+		
+		super.addNode(node);
 	}
 	
 	public function update(deltaTime:Float):Void { }
--- src/milkshake/core/Node.hx
+++ src/milkshake/core/Node.hx
@@ -1,16 +1,27 @@
 package milkshake.core;
-
-using Lambda;
+import hsl.haxe.direct.DirectSignaler.DirectSignaler;
+import hsl.haxe.Signaler.Signaler;
 
 class Node
 {
 	public var id(default, default):String;
+	public var parent(default, null):Node;
 	public var nodes(default, null):Array<Node>;
+	
+	public var onNodeAdded(default, null):Signaler<Node>;
+	public var onNodeRemoved(default, null):Signaler<Node>;
 	
 	public function new(id:String = "undefined-node")
 	{
 		this.id = id;
 		this.nodes = [];
+		
+		this.onNodeAdded = new DirectSignaler(this);
+	}
+	
+	public function setParent(parent:Node):Void
+	{
+		this.parent = parent;
 	}
 	
 	public function addNode(node:Node):Void
@@ -29,5 +40,15 @@ class Node
 		{
 			return node.id == id;
 		})[0];
+	}
+	
+	public function dispatchOnNodeAdded(node:Node):Void
+	{
+		if (parent != null)
+		{
+			parent.dispatchOnNodeAdded(node);
+		}
+		
+		onNodeAdded.dispatch(node);
 	}
 }
