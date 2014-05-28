8181927d50fdc041196d22d464c598fcde17f586
Andrew Munro <andrew.munro@dubitlimited.com>
2014-05-28 13:33:04 +0100

    Fixing node events. Fixing ship submodules space not being set when ship is added to a node.

--- src/milkshake/core/Node.hx
+++ src/milkshake/core/Node.hx
@@ -6,11 +6,13 @@ import hsl.haxe.Signaler.Signaler;
 class Node
 {
 	public var id(default, default):String;
-	public var parent(default, null):Node;
+	public var parent(default, set):Node;
 	public var nodes(default, null):Array<Node>;
 	
 	public var onNodeAdded(default, null):Signaler<Node>;
 	public var onNodeRemoved(default, null):Signaler<Node>;
+	public var onAddedToNode(default, null):Signaler<Node>;
+	public var onRemovedFromNode(default, null):Signaler<Node>;
 	
 	public function new(?id:String = null)
 	{
@@ -18,24 +20,38 @@ class Node
 		this.nodes = [];
 		
 		this.onNodeAdded = new DirectSignaler(this);
+		this.onNodeRemoved = new DirectSignaler(this);
+		this.onAddedToNode = new DirectSignaler(this);
+		this.onRemovedFromNode = new DirectSignaler(this);
 	}
 	
-	public function setParent(parent:Node):Void
+	public function set_parent(value:Node):Node
 	{
-		this.parent = parent;
+		this.parent = value;
+		if (value != null) onAddedToNode.dispatch(value);
+		return value;
 	}
 	
 	public function addNode(node:Node):Void
 	{
-		node.setParent(this);
-		
+		node.parent = this;
 		nodes.push(node);
+		
+		if (parent != null)
+		{
+			parent.onNodeAdded.dispatch(node);
+		}
+		onNodeAdded.dispatch(node);
 	}
 	
 	public function removeNode(node:Node):Void
 	{
 		if (node == null) return;
+		node.parent = null;
 		nodes.remove(node);
+		
+		onNodeRemoved.dispatch(node);
+		node.onRemovedFromNode.dispatch(this);
 	}
 	
 	public function getNodeById<T>(id:String):T
@@ -60,15 +76,5 @@ class Node
 		
 		trace("Found node with id '" + id + "'but could not cast to type '" + t + "'!");
 		return null;
-	}
-	
-	public function dispatchOnNodeAdded(node:Node):Void
-	{
-		if (parent != null)
-		{
-			parent.dispatchOnNodeAdded(node);
-		}
-		
-		onNodeAdded.dispatch(node);
 	}
 }
