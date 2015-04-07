d40095671f71178cf916f20371d0d825d6121abc
Lucas Jones <lucas.jones@dubitlimited.com>
2015-04-07 13:12:40 +0100

    Scene - Removed clear color

--- src/milkshake/game/scene/Scene.hx
+++ src/milkshake/game/scene/Scene.hx
@@ -1,6 +1,5 @@
 package milkshake.game.scene;
 
-import hsl.haxe.Signal;
 import milkshake.assets.loader.AssetLoader;
 import milkshake.core.DisplayObject;
 import milkshake.core.Graphics;
@@ -15,22 +14,13 @@ class Scene extends DisplayObject
 	public var cameras(default, null):CameraManager;
 	public var loader(default, null):AssetLoader;
 
-	var clearColor:Int;
-
 	public function new(?id:String, content:Array<String>, ?defaultCameras:Array<Camera>, clearColor:Int = Color.RED)
 	{
 		super(id);
-
-		this.clearColor = clearColor;
 
 		scene = this;
 
 		loader = new AssetLoader(content);
 		cameras = new CameraManager(defaultCameras);
-	}
-
-	override public function create():Void
-	{
-		addNode(GraphicsHelper.generateRectangle(Globals.SCREEN_WIDTH, Globals.SCREEN_HEIGHT, clearColor));
 	}
 }
