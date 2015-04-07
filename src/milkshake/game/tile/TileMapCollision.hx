4dab8681f78805d33cbfe3a9d45b86f3a7d54f80
Lucas Jones <lucas.jones@dubitlimited.com>
2015-04-07 13:12:56 +0100

    TileMap - Added basic optimize code

--- src/milkshake/game/tile/TileMapCollision.hx
+++ src/milkshake/game/tile/TileMapCollision.hx
@@ -8,14 +8,21 @@ import pixi.Rectangle;
 
 class TileMapCollision extends DisplayObject
 {
+	var tileMapData:TileMapData;
+
+	var rectanglesGrid:Array<Array<Rectangle>>;
 	var rectangles:Array<Rectangle>;
+
 	var graphics:Graphics;
 
 	public function new(tileMapData:TileMapData, tileSize:Int)
 	{
 		super();
 
+		this.tileMapData = tileMapData;
+
 		rectangles = [];
+		rectanglesGrid = [];
 
 		for (x in 0 ... tileMapData.width)
 		{
@@ -23,22 +30,98 @@ class TileMapCollision extends DisplayObject
 			{
 				var index = tileMapData.data[y][x];
 
+				if(rectanglesGrid[y] == null)
+				{
+					rectanglesGrid[y] = [];
+				}
+
 				if(index > 0)
 				{
-					rectangles.push(new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize));
+					var rectangle = new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize);
+
+					rectanglesGrid[y].push(rectangle);
+					rectangles.push(rectangle);
+				}
+				else
+				{
+					rectanglesGrid[y].push(null);
 				}
 			}
 		}
 
 		addNode(graphics = new Graphics());
+
+		cleanUp();
+	}
+
+	function cleanUp():Void
+	{
+		var delta = Date.now().getTime();
+
+		for (x in 0 ... tileMapData.width - 1)
+		{
+			for (y in 0 ... tileMapData.height)
+			{			
+				var retctangle = rectanglesGrid[y][x];
+				var index = tileMapData.data[y][x];
+
+				if(retctangle != null)
+				{
+					var rightRetctangle = rectanglesGrid[y][x + 1];
+					var rightIndex = tileMapData.data[y][x + 1];
+
+					if(rightRetctangle != null && index == rightIndex)
+					{
+						rectangles.remove(rightRetctangle);
+
+						retctangle.width += rightRetctangle.width;
+						rectanglesGrid[y][x + 1] = retctangle;
+					}					
+				}
+			}
+		}
+
+		for(rectangle in rectangles)
+		{
+			var xTiles = Math.floor(rectangle.width / 24);
+			var sameTile = true;
+			var offset = 1;
+
+			while(sameTile)
+			{
+				for (xTile in 0 ... xTiles)
+				{
+					var curRetctangle = rectanglesGrid[Math.floor(rectangle.y / 24) + offset][Math.floor(rectangle.x / 24) + xTile];
+
+					var index = tileMapData.data[Math.floor(rectangle.y / 24) + offset][Math.floor(rectangle.x / 24) + xTile];
+					var rightIndex = tileMapData.data[Math.floor(rectangle.y / 24)][Math.floor(rectangle.x / 24) + xTile];
+
+					if(curRetctangle == null || curRetctangle.width > rectangle.width || index != rightIndex) sameTile = false;
+				}
+
+				if(sameTile)
+				{
+					rectangle.height += 24;
+
+					for (xTile in 0 ... xTiles)
+					{
+						var curRetctangle = rectanglesGrid[Math.floor(rectangle.y / 24) + offset][Math.floor(rectangle.x / 24) + xTile];
+						rectangles.remove(curRetctangle);
+					}
+
+					offset++;
+				}
+			}
+		}
+
+		Console.error(Date.now().getTime() - delta);
+		
 	}
 
 	override public function update(deltaTime:Float):Void
 	{
 		graphics.clear();
 		graphics.begin(Color.RED, 0.1, 1, Color.RED);
-
-		graphics.graphics.drawRect(0, 0, 10, 10);
 
 		for(rectangle in rectangles)
 		{	
