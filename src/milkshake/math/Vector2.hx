a74db408e729766cd057f2bb14ecc105b06879c2
Andrew Munro <andrew.munro@dubitlimited.com>
2015-05-02 18:15:50 +0100

    Updating vector2 class

--- src/milkshake/math/Vector2.hx
+++ src/milkshake/math/Vector2.hx
@@ -5,6 +5,10 @@ class Vector2
 	public static var ZERO(get, never):Vector2;
 	public static var ONE(get, never):Vector2;
 	public static var HALF(get, never):Vector2;
+	public static var DOWN(get, never):Vector2;
+	public static var UP(get, never):Vector2;
+	public static var LEFT(get, never):Vector2;
+	public static var RIGHT(get, never):Vector2;
 
 	public static function get_ZERO():Vector2
 	{
@@ -21,6 +25,26 @@ class Vector2
 		return new Vector2(1, 1);
 	}
 
+	public static function get_UP():Vector2
+	{
+		return new Vector2(0, -1);
+	}
+
+	public static function get_DOWN():Vector2
+	{
+		return new Vector2(0, 1);
+	}
+
+	public static function get_LEFT():Vector2
+	{
+		return new Vector2(-1, 0);
+	}
+
+	public static function get_RIGHT():Vector2
+	{
+		return new Vector2(1, 0);
+	}
+
 	public static function EQUAL(value:Float):Vector2
 	{
 		return new Vector2(value, value);
@@ -35,19 +59,21 @@ class Vector2
 		this.y = y;
 	}
 
+	@:op(A * B)
 	public function multi(value:Vector2):Vector2
 	{
-		x *= value.x;
-		y *= value.y;
-
-		return this;
+		return new Vector2(x * value.x, y * value.y);
 	}
 
+	@:op(A * B)
+	public function multiSingle(value:Float):Vector2
+	{
+		return new Vector2(x * value, y * value);
+	}
+
+	@:op(A + B)
 	public function add(value:Vector2):Vector2
 	{
-		x += value.x;
-		y += value.y;
-
-		return this;
+		return new Vector2(x + value.x, y + value.y);
 	}
 }
