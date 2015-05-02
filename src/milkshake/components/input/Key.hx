4bdb5417c934f2fb30d47763885ddb2ebd86dbf6
Andrew Munro <andrew.munro@dubitlimited.com>
2015-05-02 20:59:21 +0100

    Adding wasd keys

--- src/milkshake/components/input/Key.hx
+++ src/milkshake/components/input/Key.hx
@@ -6,6 +6,10 @@ class Key
 	public static inline var DOWN = 40;
 	public static inline var LEFT = 37;
 	public static inline var RIGHT = 39;
+	public static inline var A = 65;
+	public static inline var W = 87;
+	public static inline var D = 68;
+	public static inline var S = 83;
 	public static inline var SHIFT = 16;
 	public static inline var SPACE = 32;
 }
