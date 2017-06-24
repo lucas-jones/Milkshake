a20d8afd38cb1c822fd9ba27e7d5c22157db34d0
Andrew Munro <andrew.munro@skybettingandgaming.com>
2017-06-24 05:42:11 +0100

    Fixing particleEmitter update

--- src/milkshake/core/ParticleEmitter.hx
+++ src/milkshake/core/ParticleEmitter.hx
@@ -9,19 +9,26 @@ class ParticleEmitter extends DisplayObject
 
 	public var emitting:Bool = true;
 
+	var elapsed:Date;
+
 	public function new(textures:Array<Texture>, id:String="undefined-particle-emitter", ?config:Dynamic)
 	{
 		super(id);
 
 		emitter = new Emitter(displayObject, textures, config);
 		emitter.emit = true;
+
+		elapsed = Date.now();
 	}
 
 	override public function update(delta:Float)
 	{
+		var now = Date.now();
+
 		if(emitting)
 		{
-			emitter.update(delta);
+			emitter.update((now.getTime() - elapsed.getTime()) * 0.001);
+			elapsed = now;
 		}
 		super.update(delta);
 	}
