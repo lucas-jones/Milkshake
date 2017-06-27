0d8c1e598ce11bc6023d86609274ad54ccc72b7b
Andrew Munro <andrew.munro@skybettingandgaming.com>
2017-06-27 14:24:46 +0100

    Initial commit (HARVEST)

--- src/milkshake/core/Text.hx
+++ src/milkshake/core/Text.hx
@@ -6,15 +6,21 @@ class Text extends DisplayObject
 {
 	var text:pixi.core.text.Text;
 
-	public function new(message:String = "")
+	public function new(message:String = "", color:String = "black")
 	{
 		super();
 
 		displayObject.addChild(text = new pixi.core.text.Text(message));
+		setColor(color);
 	}
 
 	public function setText(message:String)
 	{
 		text.text = message;
-	}	
+	}
+
+	public function setColor(color:String)
+	{
+		text.style.fill = color;
+	}
 }
